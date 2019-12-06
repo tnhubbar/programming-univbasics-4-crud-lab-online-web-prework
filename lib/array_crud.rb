8343a2b6538389dab7d35ea6e097a2dd40203d94
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-06 02:00:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,14 @@
 
+# retrieve_first_element_from_array
+# This method takes in one argument, the array from which we want to retrieve an element. Use [] notation to return the value stored at the first index of the array. Remember that arrays are zero-indexed. This means that the first index number is 0 and it counts up from there. So, the first element of an array is stored at index 0.
 
+# retrieve_last_element_from_array
+# This method takes in one argument, the array from which we want to retrieve an element. There are a number of ways to do this, but we recommend using the [] method with the following hint:
+
+# The last element of an array is considered to be stored at an index of -1.
+
+# update_element_from_index
+# This method takes in three arguments: the array from which we want to update an element, the index number of the element to update and the new element value. Refer back to your retrieve_element_from_index method for how to target the desired element.
 def create_an_empty_array
   []  
 end
